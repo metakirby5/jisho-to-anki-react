d4f9d2e7858de3b77145025eaf0fa41c4074d1b0
Ethan Chan <metakirby5@gmail.com>
2021-09-04 17:27:09 +0900

    Oh no, I should have used yarn...

---
+++ src/Jisho.tsx
@@ -0,0 +1,3 @@
+export default class Jisho {
+    readonly JISHO_SEARCH = 'https://jisho.org/api/v1/search/words?keyword={0}'
+}
---
+++ src/Types.tsx
@@ -0,0 +1,6 @@
+export interface Results {
+    readonly url: string
+    readonly word: string
+    readonly reading: string
+    readonly meaning: string
+}
